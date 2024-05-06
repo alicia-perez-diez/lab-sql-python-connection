c2e8bbb775fda8ebc75a53f777f0e484aac88405
alicia-perez-diez <perezdiezalicia9@gmail.com>
2024-05-06 13:59:32 +0200

    V_1

--- rental_table.sql
+++ rental_table.sql
@@ -1,5 +1,4 @@
 USE sakila;
 -
-CREATE TEMPORARY TABLE rental_m_y
-SELECT rental_id, rental_date, MONTH(r.rental_date) AS rental_month, YEAR(r.rental_date) AS rental_year, inventory_id, customer_id, return_date, staff_id, last_update
+SELECT rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update
 FROM rental AS r;
